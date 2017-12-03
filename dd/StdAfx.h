1da1158d49376ee443ee622736497be109a41e12
lysgwl <lysgwl@163.com>
2017-12-03 20:00:59 +0800

    2017.12.03 20:00

--- dd/StdAfx.h
+++ dd/StdAfx.h
@@ -15,11 +15,25 @@
 #include <afxcmn.h>			// MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
+#include <afxcontrolbars.h>     // MFC support for ribbons and control bars
+
 //////////////////////////////////////////////////////////////////////////
 //
 #pragma warning(disable : 4786)		//必须放在vector之前,屏蔽C4786警告
 
-#include "Inc/PubClass.h"
 #include <afxdlgs.h>
+#include "Inc/PubClass.h"
+
+#ifndef _UNICODE
+#if defined _M_IX86
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_IA64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+#endif
 
 #endif  
